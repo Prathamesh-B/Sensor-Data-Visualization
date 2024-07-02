b67df5d38b9e86af9cb409316c7cc9048b7958ec
Riya Torgal <142211656+RiyaTorgal@users.noreply.github.com>
2024-07-02 22:35:11 +0530

    Data (#10)

--- src/data.js
+++ src/data.js
@@ -293,7 +293,7 @@ export const cards = [
 
 export const DTdata = [
     { id: 1, machine_id: 1, machine_nm: "Mitsubishi 35", dwt_type: "Outage", start: "2024-06-01T02:45:00Z", end: "2024-06-01T03:30:00Z", lost: "45", rc: "Sensor Failure", fm: "Rahul Singh", shift: "Afternoon" },
-    { id: 2, machine_id: 2, machine_nm: "Laser Cutter", dwt_type: "Outage", start: "2024-06-03T02:45:00Z", end: "2024-06-03T03:30:00Z", lost: "45", rc: "Sensor Failure", fm: "Rohit Anand", shift: "Afternoon" },
+    { id: 2, machine_id: 2, machine_nm: "Laser Cutter", dwt_type: "Outage", start: "2024-06-03T02:45:00Z", end: "2024-06-03T03:30:00Z", lost: "45", rc: "Sensor Failure", fm: "Ashish Kumar", shift: "Afternoon" },
     { id: 3, machine_id: 3, machine_nm: "Stratos 42", dwt_type: "Maintenance", start: "2024-06-04T02:45:00Z", end: "2024-06-04T03:30:00Z", lost: "45", rc: "Sensor Failure", fm: "Rahul Singh", shift: "Afternoon" },
     { id: 4, machine_id: 4, machine_nm: "Falcon 65", dwt_type: "Maintenance", start: "2024-06-05T02:45:00Z", end: "2024-06-05T03:30:00Z", lost: "45", rc: "Sensor Failure", fm: "Nilesh Chutke", shift: "Afternoon" },
     { id: 5, machine_id: 5, machine_nm: "Cypher 15", dwt_type: "Outage", start: "2024-06-06T02:45:00Z", end: "2024-06-06T03:30:00Z", lost: "45", rc: "Sensor Failure", fm: "Rahul Singh", shift: "Afternoon" }

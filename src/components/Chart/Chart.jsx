e60f84328b397176dbbd98244e9e0c16a3bf829c
Riya Torgal <142211656+RiyaTorgal@users.noreply.github.com>
2024-07-25 10:56:57 +0530

    Add Chart (#19)

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -7,6 +7,12 @@ import {
     Tooltip,
     ResponsiveContainer,
     Brush,
+    PieChart, 
+    Pie, 
+    Cell,
+    Legend,
+    Bar,
+    BarChart,
 } from "recharts";
 import { formatDate } from "../../utils";
 
@@ -60,3 +66,85 @@ const Chart = ({ data }) => {
 };
 
 export default Chart;
+
+
+const BD = [
+    {
+      name: 'Availability',
+      val: 65,
+
+    },
+    {
+      name: 'Efficiency',
+      val: 100,
+
+    },
+    {
+      name: 'Quality',
+      val: 70,
+
+    },
+  ];
+  
+  export const BarGraph = () => {
+  
+      return (
+        <>
+        <ResponsiveContainer width={350} height={350} className="chart">
+          <BarChart
+            width={400}
+            height={200}
+            data={BD}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="4 4" />
+            <XAxis dataKey="name" />
+            <YAxis 
+            label={{
+              offset: -20,
+            }}
+            />
+            <Tooltip />
+            <Bar dataKey="val" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+        </>
+      );
+  }
+
+  const PD = [
+    { name: 'Active', value: 140 },
+    { name: 'Down', value: 10 },
+  ];
+
+  const COLORS = [ '#0088FE', '#FF8042'];
+  
+  export  const PieGraph = () => {
+  
+      return (
+        <ResponsiveContainer width={400} height={400} className="chart">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={PD}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              outerRadius={140}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {PD.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+            <Legend />
+          </PieChart>
+        </ResponsiveContainer>
+      );
+  }
+  
